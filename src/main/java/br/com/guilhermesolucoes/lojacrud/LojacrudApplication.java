c638058ef3fa13d8fdc24a5f4cb2b8e48703d054
Guilherme Pacheco <Guilherme.p.medeiros@gmail.com>
2023-03-14 13:38:59 -0300

    Edit to work

--- src/main/java/br/com/guilhermesolucoes/lojacrud/LojacrudApplication.java
+++ src/main/java/br/com/guilhermesolucoes/lojacrud/LojacrudApplication.java
@@ -2,13 +2,14 @@ package br.com.guilhermesolucoes.lojacrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author Guilherme Pacheco De Medeiros
  * @version 1.0
  */
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class LojacrudApplication {
 
 	//Método main
